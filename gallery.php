680ab3bddb5c036d96b23465e2eda82d6e43ea29
erlindaainun <46706599+erlindaainun@users.noreply.github.com>
2021-06-26 20:39:25 +0700

    fix bug

--- gallery.php
+++ gallery.php
@@ -67,7 +67,7 @@
 
         <div class="row portfolio-container aos-init aos-animate" data-aos="fade-up" style="position: relative; height: 891px;">
 
-          <div class="col-lg-4 col-md-6 portfolio-item filter-app" style="position: absolute; left: 0px; top: 0px;">
+          <div class="col-lg-4 col-md-6 portfolio-item filter-u6-u8" style="position: absolute; left: 0px; top: 0px;">
             <div class="portfolio-wrap">
               <img src="assets/img/portfolio/portfolio-1.jpg" class="img-fluid" alt="">
               <div class="portfolio-info">
@@ -81,7 +81,7 @@
             </div>
           </div>
 
-          <div class="col-lg-4 col-md-6 portfolio-item filter-web" style="position: absolute; left: 380px; top: 0px;">
+          <div class="col-lg-4 col-md-6 portfolio-item filter-u6-u8" style="position: absolute; left: 380px; top: 0px;">
             <div class="portfolio-wrap">
               <img src="assets/img/portfolio/portfolio-2.jpg" class="img-fluid" alt="">
               <div class="portfolio-info">
@@ -95,7 +95,7 @@
             </div>
           </div>
 
-          <div class="col-lg-4 col-md-6 portfolio-item filter-app" style="position: absolute; left: 760px; top: 0px;">
+          <div class="col-lg-4 col-md-6 portfolio-item filter-u6-u8" style="position: absolute; left: 760px; top: 0px;">
             <div class="portfolio-wrap">
               <img src="assets/img/portfolio/portfolio-3.jpg" class="img-fluid" alt="">
               <div class="portfolio-info">
@@ -109,7 +109,7 @@
             </div>
           </div>
 
-          <div class="col-lg-4 col-md-6 portfolio-item filter-card" style="position: absolute; left: 0px; top: 297px;">
+          <div class="col-lg-4 col-md-6 portfolio-item filter-u6-u8" style="position: absolute; left: 0px; top: 297px;">
             <div class="portfolio-wrap">
               <img src="assets/img/portfolio/portfolio-4.jpg" class="img-fluid" alt="">
               <div class="portfolio-info">
@@ -123,7 +123,7 @@
             </div>
           </div>
 
-          <div class="col-lg-4 col-md-6 portfolio-item filter-web" style="position: absolute; left: 380px; top: 297px;">
+          <div class="col-lg-4 col-md-6 portfolio-item filter-u6-u8" style="position: absolute; left: 380px; top: 297px;">
             <div class="portfolio-wrap">
               <img src="assets/img/portfolio/portfolio-5.jpg" class="img-fluid" alt="">
               <div class="portfolio-info">
@@ -137,7 +137,7 @@
             </div>
           </div>
 
-          <div class="col-lg-4 col-md-6 portfolio-item filter-app" style="position: absolute; left: 760px; top: 297px;">
+          <div class="col-lg-4 col-md-6 portfolio-item filter-u6-u8" style="position: absolute; left: 760px; top: 297px;">
             <div class="portfolio-wrap">
               <img src="assets/img/portfolio/portfolio-6.jpg" class="img-fluid" alt="">
               <div class="portfolio-info">
@@ -151,7 +151,7 @@
             </div>
           </div>
 
-          <div class="col-lg-4 col-md-6 portfolio-item filter-card" style="position: absolute; left: 0px; top: 594px;">
+          <div class="col-lg-4 col-md-6 portfolio-item filter-u6-u8" style="position: absolute; left: 0px; top: 594px;">
             <div class="portfolio-wrap">
               <img src="assets/img/portfolio/portfolio-7.jpg" class="img-fluid" alt="">
               <div class="portfolio-info">
@@ -165,7 +165,7 @@
             </div>
           </div>
 
-          <div class="col-lg-4 col-md-6 portfolio-item filter-card" style="position: absolute; left: 380px; top: 594px;">
+          <div class="col-lg-4 col-md-6 portfolio-item filter-u6-u8" style="position: absolute; left: 380px; top: 594px;">
             <div class="portfolio-wrap">
               <img src="assets/img/portfolio/portfolio-8.jpg" class="img-fluid" alt="">
               <div class="portfolio-info">
@@ -179,7 +179,7 @@
             </div>
           </div>
 
-          <div class="col-lg-4 col-md-6 portfolio-item filter-web" style="position: absolute; left: 760px; top: 594px;">
+          <div class="col-lg-4 col-md-6 portfolio-item filter-u6-u8" style="position: absolute; left: 760px; top: 594px;">
             <div class="portfolio-wrap">
               <img src="assets/img/portfolio/portfolio-9.jpg" class="img-fluid" alt="">
               <div class="portfolio-info">
