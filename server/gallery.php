e8312c1106beb38468126bac3d14dd0090722c2a
VREEdom <35516476+vonsogt@users.noreply.github.com>
2021-07-02 02:18:30 +0700

    change footer info

--- server/gallery.php
+++ server/gallery.php
@@ -449,7 +449,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
     <footer class="main-footer">
       <!-- To the right -->
       <div class="float-right d-none d-sm-inline">
-        Anything you want
+        Erlinda Ainun
       </div>
       <!-- Default to the left -->
       <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights reserved.
