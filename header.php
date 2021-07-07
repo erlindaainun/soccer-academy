5787d8d3344843910ed240d60cfd43e7dd0daf5e
erlindaainun <46706599+erlindaainun@users.noreply.github.com>
2021-07-07 23:29:22 +0700

    wip

--- header.php
+++ header.php
@@ -39,15 +39,24 @@
                 <li class="dropdown">
                     <a class="<?php if (
                                     $_SERVER['SCRIPT_NAME'] == "/registration-team.php"
-                                    || $_SERVER['SCRIPT_NAME'] == "/match-schedule.php"
+                                    || $_SERVER['SCRIPT_NAME'] == "/schedule.php"
                                     || $_SERVER['SCRIPT_NAME'] == "/standing.php"
+                                ) echo "active" ?>" href="#"><span>Turnamen</span> <i class="bi bi-chevron-down"></i>
+                    </a>
+                    <ul>
+                        <li><a class="<?php if ($_SERVER['SCRIPT_NAME'] == "//registration-team.php") echo "active" ?>" href="/registration-team.php">Registrasi Tim</a></li>
+                        <li><a class="<?php if ($_SERVER['SCRIPT_NAME'] == "/schedule.php") echo "active" ?>" href="schedule.php">Jadwal Pertandingan</a></li>
+                        <li><a class="<?php if ($_SERVER['SCRIPT_NAME'] == "/standing.php") echo "active" ?>" href="standing.php">Klasemen</a></li>
+                    </ul>
+                </li>
+                <li class="dropdown">
+                    <a class="<?php if (
+                                    $_SERVER['SCRIPT_NAME'] == "/registration-team.php"
                                     || $_SERVER['SCRIPT_NAME'] == "/match-scheme.php"
                                 ) echo "active" ?>" href="#"><span>Liga</span> <i class="bi bi-chevron-down"></i>
                     </a>
                     <ul>
-                        <li><a class="<?php if ($_SERVER['SCRIPT_NAME'] == "/registration-team.php") echo "active" ?>" href="registration-team.php">Registrasi Tim</a></li>
-                        <li><a class="<?php if ($_SERVER['SCRIPT_NAME'] == "/match-schedule.php") echo "active" ?>" href="match-schedule.php">Jadwal Pertandingan</a></li>
-                        <li><a class="<?php if ($_SERVER['SCRIPT_NAME'] == "/standing.php") echo "active" ?>" href="standing.php">Klasemen</a></li>
+                        <li><a class="<?php if ($_SERVER['SCRIPT_NAME'] == "/registration-team.php") echo "active" ?>" href="/registration-team.php">Registrasi Tim</a></li>
                         <li><a class="<?php if ($_SERVER['SCRIPT_NAME'] == "/match-scheme.php") echo "active" ?>" href="match-scheme.php">Skema Pertandingan</a></li>
                     </ul>
                 </li>
---
+++ schedule.php
@@ -0,0 +1,151 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Jadwal Pertandingan | SSB Tunas Jaya Duriangkang Official Website</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <?php include 'connection.php'; ?>
+
+  <!-- Favicons -->
+  <link href="assets/img/favicon.ico" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+
+  <!-- =======================================================
+  * Template Name: Vesperr - v4.2.0
+  * Template URL: https://bootstrapmade.com/vesperr-free-bootstrap-template/
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+</head>
+
+<body>
+
+  <!-- ======= Header ======= -->
+  <?php include 'header.php'; ?>
+
+  <main id="main">
+
+    <!-- ======= Breadcrumbs ======= -->
+    <section id="breadcrumbs" class="breadcrumbs">
+      <div class="container">
+
+        <div class="d-flex justify-content-between align-items-center">
+          <ol>
+            <li><a href="/">Beranda</a></li>
+            <li>Jadwal Pertandingan</li>
+          </ol>
+        </div>
+
+      </div>
+    </section><!-- End Breadcrumbs -->
+
+    <!-- ======= Portfolio Section ======= -->
+    <section id="faq" class="faq">
+      <div class="container">
+
+        <div class="section-title" data-aos="fade-up">
+          <h2>Jadwal Pertandingan</h2>
+        </div>
+
+        <div class="row middle faq-header d-flex" data-aos="fade-up">
+          <div class="col-lg-12">
+            <p>Kamis,12 April 2021</p>
+          </div>
+        </div>
+        <div class="row middle faq-item d-flex" data-aos="fade-up">
+          <div class="col-lg-3">
+            <a>Liga Sepak Bola Kota Batam</a>
+          </div>
+          <div class="col-lg-1">
+            <p>Indonesia</p>
+          </div>
+          <div class="col-lg-1">
+            <img src="assets/img/indonesia.png" width="50px">
+          </div>
+          <div class="col-lg-2">
+            <h3>0 - 5</h3>
+          </div>
+          <div class="col-lg-1">
+            <img src="assets/img/indonesia.png" width="50px">
+          </div>
+          <div class="col-lg-1">
+            <p>Indonesia</p>
+          </div>
+          <div class="col-lg-3">
+            <h5><img src="assets/img/stadium.png"> Temenggung Abdul Jamal</h5>
+          </div>
+        </div><!-- End F.A.Q Item-->
+
+        <div class="row middle faq-header d-flex" data-aos="fade-up">
+          <div class="col-lg-12">
+            <p>Kamis,12 April 2021</p>
+          </div>
+        </div>
+        <div class="row middle faq-item d-flex" data-aos="fade-up">
+          <div class="col-lg-3">
+            <a>Liga Sepak Bola Kota Batam</a>
+          </div>
+          <div class="col-lg-1">
+            <p>Indonesia</p>
+          </div>
+          <div class="col-lg-1">
+            <img src="assets/img/indonesia.png" width="50px">
+          </div>
+          <div class="col-lg-2">
+            <h3>0 - 5</h3>
+          </div>
+          <div class="col-lg-1">
+            <img src="assets/img/indonesia.png" width="50px">
+          </div>
+          <div class="col-lg-1">
+            <p>Indonesia</p>
+          </div>
+          <div class="col-lg-3">
+            <h5><img src="assets/img/stadium.png"> Temenggung Abdul Jamal</h5>
+          </div>
+        </div><!-- End F.A.Q Item-->
+
+    </section>
+
+  </main><!-- End #main -->
+
+  <!-- ======= Footer ======= -->
+  <?php include 'footer.php'; ?>
+
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/aos/aos.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+  <script src="assets/vendor/purecounter/purecounter.js"></script>
+  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
